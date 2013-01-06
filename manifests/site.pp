583dedbf67ce39c27d79636fa0e801c10b7d2789
Simon Rozet <simon@rozet.name>
2013-01-07 00:53:30 +0100

    uninstall unecessary modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,37 +44,7 @@ Service {
 }
 
 node default {
-  # core modules, needed for most things
-  include dnsmasq
   include git
-  include hub
-  include nginx
-  include nvm
-  include rbenv
-
-  # fail if FDE is not enabled
-  if $::root_encrypted == false {
-    fail('Please enable full disk encryption and try again')
-  }
-
-  # node versions
-  include nodejs::0-4
-  include nodejs::0-6
-  include nodejs::0-8
-
-  # default ruby versions
-  include ruby::1-8-7
-  include ruby::1-9-2
-  include ruby::1-9-3
-
-  # common, useful packages
-  package {
-    [
-      'ack',
-      'findutils',
-      'gnu-tar'
-    ]:
-  }
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
