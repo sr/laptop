037cac1b80a86cfea482203d47f0ce2bb468e9cd
Simon Rozet <simon@rozet.name>
2014-05-15 18:05:16 +0200

    remove rubies i don't need and install bundler for all of them

--- modules/people/manifests/sr.pp
+++ modules/people/manifests/sr.pp
@@ -55,4 +55,14 @@ class people::sr {
   file { "/Users/${::boxen_user}/tmp":
     ensure => directory;
   }
+
+  # rubies
+  ruby::version { '1.9.3': }
+  ruby::version { '2.0.0': }
+
+  ruby_gem { 'bundler':
+    gem          => 'bundler',
+    version      => '~>1.6',
+    ruby_version => '*',
+  }
 }
