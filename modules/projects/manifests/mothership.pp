4c5fb223f01a86d7aa1b6cecc62115d131607f77
Simon Rozet <me@simonrozet.com>
2014-10-28 01:04:54 +0100

    setup mothership manifest as a symlink

--- modules/projects/manifests/mothership.pp
+++ modules/projects/manifests/mothership.pp
@@ -1,17 +1,1 @@
-class projects::mothership {
-  $srcdir = "${::boxen_srcdir}/codeship"
-
-  file { $srcdir:
-    ensure => directory,
-  }
-
-  boxen::project { 'mothership':
-    source     => 'codeship/mothership',
-    dir        => "${srcdir}/mothership",
-    nginx      => true,
-    postgresql => true,
-    redis      => true,
-    ruby       => '2.1.3',
-    require    => File[$srcdir],
-  }
-}
+/Users/sr/src/codeship/mothership/doc/mothership.pp
