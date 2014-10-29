1edf240cd7068eca3a099f1dc341faec23d26d44
Simon Rozet <me@simonrozet.com>
2014-10-29 02:30:37 +0100

    Revert "setup mothership manifest as a symlink"
    This reverts commit 4c5fb223f01a86d7aa1b6cecc62115d131607f77.

--- modules/projects/manifests/mothership.pp
+++ modules/projects/manifests/mothership.pp
@@ -1,1 +1,17 @@
-/Users/sr/src/codeship/mothership/doc/mothership.pp
+class projects::mothership {
+  $srcdir = "${::boxen_srcdir}/codeship"
+
+  file { $srcdir:
+    ensure => directory,
+  }
+
+  boxen::project { 'mothership':
+    source     => 'codeship/mothership',
+    dir        => "${srcdir}/mothership",
+    nginx      => true,
+    postgresql => true,
+    redis      => true,
+    ruby       => '2.1.3',
+    require    => File[$srcdir],
+  }
+}
