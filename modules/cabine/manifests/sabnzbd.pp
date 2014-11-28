f8c22fc694250e9522b3b46ea7bd9fdbd6cc512b
Simon Rozet <me@simonrozet.com>
2014-11-28 17:10:38 +0100

    configure sabnzbd via ini_settings as much as possible to make updating easier

--- modules/cabine/manifests/sabnzbd.pp
+++ modules/cabine/manifests/sabnzbd.pp
@@ -1,11 +1,20 @@
 class cabine::sabnzbd {
-    require python
+    # sabnzbd.ini variables
+    $logdir = $cabine::config::sabnzbd_logdir
+    $admindir = $cabine::config::sabnzbd_admindir
+    $downloaddir = $cabine::config::sabnzbd_incompletedir
+    $completedir = $cabine::config::sabnzbd_completedir
+    $scriptdir = $cabine::config::sabnzbd_autoprocessdir
+    $nzbkey = $cabine::config::sabnzbd_nzbkey
+    $apikey = $cabine::config::sabnzbd_apikey
 
+    # daemon template variables
     $repodir = $cabine::config::sabnzbd_repodir
     $configdir = $cabine::config::sabnzbd_configdir
-    $logdir = $cabine::config::sabnzbd_logdir
     $configfile = "${configdir}/config.ini"
     $executable = "${repodir}/SABnzbd.py"
+
+    require python
 
     package {
         'pysqlite':
@@ -26,9 +35,48 @@ class cabine::sabnzbd {
     }
 
     file { $configfile:
-        content => template('cabine/sabnzbd.ini.erb'),
-        notify  => Service['dev.sabnzbd'],
+        content => template('cabine/sabnzbd2.ini.erb'),
+        replace => false,
         require => File[$configdir],
+        notify  => Service['dev.sabnzbd'],
+    }
+
+    Ini_setting {
+      path => $configfile,
+      ensure => present,
+      require => File[$configfile],
+      notify  => Service['dev.sabnzbd'],
+    }
+
+    ini_setting {
+      'log_dir':
+        section => 'misc',
+        setting => 'log_dir',
+        value => $logdir;
+      'admin_dir':
+        section => 'misc',
+        setting => 'admin_dir',
+        value   => $admindir;
+      'download_dir':
+        section => 'misc',
+        setting => 'download_dir',
+        value   => $downloaddir;
+      'complete_dir':
+        section => 'misc',
+        setting => 'complete_dir',
+        value   => $completedir;
+      'script_dir':
+        section => 'misc',
+        setting => 'script_dir',
+        value   => $scriptdir;
+      'nzb_key':
+        section => 'misc',
+        setting => 'nzb_key',
+        value   => $nzbkey;
+      'api_key':
+        section => 'misc',
+        setting => 'api_key',
+        value   => $apikey;
     }
 
     file { '/Library/LaunchDaemons/dev.sabnzbd.plist':
