1e1503e606a46733c5cc3e3aaea872102bc27675
Simon Rozet <me@simonrozet.com>
2014-11-28 19:00:23 +0100

    disable sickbeard and plex for now

--- modules/cabine/manifests/init.pp
+++ modules/cabine/manifests/init.pp
@@ -2,11 +2,5 @@ class cabine {
     require boxen::config
     require cabine::config
     require cabine::sabnzbd
-    require cabine::sickbeard
     require cabine::couchpotato
-    require cabine::plex
-
-    package { 'python':
-        ensure => '2.7.3'
-    }
 }
