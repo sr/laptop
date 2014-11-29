25a81e4824d92c43d8e5edd140cbc2d9cc83df39
Simon Rozet <me@simonrozet.com>
2014-11-29 19:04:58 +0100

    fix ident of plex

--- modules/cabine/manifests/plex.pp
+++ modules/cabine/manifests/plex.pp
@@ -1,10 +1,10 @@
 class cabine::plex {
-    package {
-        'Plex Media Server':
-            provider => 'appdmg',
-            source   => 'https://downloads.plex.tv/plex-media-server/0.9.9.14.531-7eef8c6/PlexMediaServer-0.9.9.14.531-7eef8c6-OSX.zip';
-        'Plex Home Theater':
-            provider => 'compressed_app',
-            source   => 'https://downloads.plex.tv/plex-home-theater/1.2.2.331-2d6426d7/PlexHomeTheater-1.2.2.331-2d6426d7-macosx-x86_64.zip';
-    }
+  package {
+    'Plex Media Server':
+      provider => 'appdmg',
+      source   => 'https://downloads.plex.tv/plex-media-server/0.9.9.14.531-7eef8c6/PlexMediaServer-0.9.9.14.531-7eef8c6-OSX.zip';
+    'Plex Home Theater':
+      provider => 'compressed_app',
+      source   => 'https://downloads.plex.tv/plex-home-theater/1.2.2.331-2d6426d7/PlexHomeTheater-1.2.2.331-2d6426d7-macosx-x86_64.zip';
+  }
 }
