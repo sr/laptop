1735f17b03b1cc911c07538f7deec7b07c93eccc
Simon Rozet <me@simonrozet.com>
2014-12-16 11:21:50 +0100

    sync mothership.pp

--- modules/projects/manifests/mothership.pp
+++ modules/projects/manifests/mothership.pp
@@ -1,10 +1,17 @@
 class projects::mothership {
+  $srcdir = "${::boxen_srcdir}/codeship"
+
+  file { $srcdir:
+    ensure => directory,
+  }
+
   boxen::project { 'mothership':
     source     => 'codeship/mothership',
-    dir        => "${::boxen::config::srcdir}/codeship/mothership",
+    dir        => "${srcdir}/mothership",
     nginx      => true,
     postgresql => true,
     redis      => true,
-    ruby       => '2.1.4',
+    ruby       => '2.1.5',
+    require    => File[$srcdir],
   }
 }
