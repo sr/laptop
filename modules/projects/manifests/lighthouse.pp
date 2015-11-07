8098530d830fe38e74d4a3ece663475811f6dfb2
Simon Rozet <me@simonrozet.com>
2015-11-07 14:20:53 +0100

    :fire: codeship projects

--- modules/projects/manifests/lighthouse.pp
+++
@@ -1,9 +0,0 @@
-class projects::lighthouse {
-  boxen::project { 'lighthouse':
-    source     => 'codeship/lighthouse',
-    dir        => "${::boxen::config::srcdir}/lighthouse",
-    nginx      => true,
-    redis      => true,
-    ruby       => '2.1.5',
-  }
-}
