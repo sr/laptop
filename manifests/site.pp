dc07788ed1d14bbc3724835327738f17d57d5e4a
Simon Rozet <me@simonrozet.com>
2015-12-12 17:13:16 +0100

    stop installing boxen's git-credentials helper config

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,10 +39,7 @@ Repository {
   extra    => [
     '--recurse-submodules'
   ],
-  require  => File["${boxen::config::bindir}/boxen-git-credential"],
-  config   => {
-    'credential.helper' => "${boxen::config::bindir}/boxen-git-credential"
-  }
+  require  => File["${boxen::config::bindir}/boxen-git-credential"]
 }
 
 Service {
