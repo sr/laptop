d80b5349fbd81cf451dd8c99adcb6531b276f9ee
Simon Rozet <me@simonrozet.com>
2015-12-12 17:13:31 +0100

    clean up personnal manifest to only assume one machine

--- modules/people/manifests/sr.pp
+++ modules/people/manifests/sr.pp
@@ -14,65 +14,54 @@ class people::sr {
     parameters => ['timestamp_timeout=1440'],
   }
 
-  case $::hostname {
-    'frida': {
-      include chrome
-      include dropbox
-      include screen
+  include chrome
+  include dropbox
+  include screen
+  include xquartz
 
-      package {
-        'Backblaze':
-          provider => 'appdmg',
-          source   => 'https://secure.backblaze.com/mac/install_backblaze.dmg';
-        'Kindle':
-          provider => 'appdmg',
-          source   => 'http://kindleformac.amazon.com/40961/KindleForMac.dmg';
-        'OmniFocus':
-          provider => 'appdmg_eula',
-          source   => 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.10/OmniFocus-2.2.4.dmg';
-        'VyprVPN':
-          provider => 'appdmg',
-          source   => 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/2.6.5.2546/VyprVPN_v2.6.5.2546.dmg';
-      }
+  package {
+    'Backblaze':
+      provider => 'appdmg',
+      source   => 'https://secure.backblaze.com/mac/install_backblaze.dmg';
+    'Kindle':
+      provider => 'appdmg',
+      source   => 'http://kindleformac.amazon.com/40961/KindleForMac.dmg';
+    'OmniFocus':
+      provider => 'appdmg_eula',
+      source   => 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.10/OmniFocus-2.2.4.dmg';
+    'VyprVPN':
+      provider => 'appdmg',
+      source   => 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/2.6.5.2546/VyprVPN_v2.6.5.2546.dmg';
+  }
 
-      package { [
-        'bash-completion',
-        'mosh',
-        'namebench',
-        'pstree',
-        'ssh-copy-id',
-        'tarsnap',
-        'tig',
-        'tree',
-      ]: }
+  package { [
+    'bash-completion',
+    'mosh',
+    'namebench',
+    'pstree',
+    'ssh-copy-id',
+    'tarsnap',
+    'tig',
+    'tree',
+  ]: }
 
-      $hosts = [
-        'facebook.com',
-        'www.facebook.com',
-        'twitter.com',
-        'www.twitter.com',
-        'arseblog.com',
-        'news.arseblog.com',
-        'theguardian.com',
-        'www.theguardian.com',
-      ]
+  $hosts = [
+    'facebook.com',
+    'www.facebook.com',
+    'twitter.com',
+    'www.twitter.com',
+    'arseblog.com',
+    'news.arseblog.com',
+    'theguardian.com',
+    'www.theguardian.com',
+  ]
 
-      host { $hosts:
-        ensure => present,
-        ip     => '127.0.0.1',
-      }
+  host { $hosts:
+    ensure => present,
+    ip     => '127.0.0.1',
+  }
 
-      file { ['/opt/tarsnap', '/opt/tarsnap/cache']:
-        ensure => directory
-      }
-    }
-
-    'dana': {
-      notice('dana')
-    }
-
-    default: {
-      fail("Unknown machine: ${::hostname}")
-    }
+  file { ['/opt/tarsnap', '/opt/tarsnap/cache']:
+    ensure => directory
   }
 }
