6e40270a89b6d5584e2f1728e2db487be254e9b1
Simon Rozet <me@simonrozet.com>
2015-12-13 21:02:56 +0100

    switch to hier based configuration

--- modules/people/manifests/sr.pp
+++ modules/people/manifests/sr.pp
@@ -1,46 +1,6 @@
 class people::sr {
-  include osx::dock::autohide
-  include osx::finder::empty_trash_securely
-  include osx::finder::unhide_library
-  include osx::disable_app_quarantine
-  include osx::no_network_dsstores
-  include osx::keyboard::capslock_to_control
-
-  $boxen_bin = "${::boxen_home}/bin"
-  $home = "/Users/${::boxen_user}"
-
   sudoers { "Defaults@${::hostname}":
     type       => 'default',
     parameters => ['timestamp_timeout=1440'],
   }
-
-  include chrome
-  include dropbox
-  include screen
-  include xquartz
-
-  package {
-    'Backblaze':
-      provider => 'appdmg',
-      source   => 'https://secure.backblaze.com/mac/install_backblaze.dmg';
-    'Kindle':
-      provider => 'appdmg',
-      source   => 'http://kindleformac.amazon.com/40961/KindleForMac.dmg';
-    'OmniFocus':
-      provider => 'appdmg_eula',
-      source   => 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.10/OmniFocus-2.2.4.dmg';
-    'VyprVPN':
-      provider => 'appdmg',
-      source   => 'http://www.goldenfrog.com/downloads/vyprvpn/desktop/mac/production/2.6.5.2546/VyprVPN_v2.6.5.2546.dmg';
-  }
-
-  package { [
-    'bash-completion',
-    'mosh',
-    'namebench',
-    'pstree',
-    'ssh-copy-id',
-    'tig',
-    'tree',
-  ]: }
 }
